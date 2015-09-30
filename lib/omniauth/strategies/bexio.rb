c5f32eab147a2b2b382b738388f68895e5c2b934
Igor Springer <springerigor@gmail.com>
2015-09-30 08:38:22 +0200

    Add company_profile to extra hash

--- lib/omniauth/strategies/bexio.rb
+++ lib/omniauth/strategies/bexio.rb
@@ -22,8 +22,18 @@ module OmniAuth
         }
       end
 
+      extra do
+        {
+          company_profile: company_profile
+        }
+      end
+
       def raw_info
         @raw_info ||= access_token.get("/api2.php/#{ access_token.params['org'] }/user/#{ access_token.params['user_id'] }", :headers => { 'Accept' => 'application/json' }).parsed
+      end
+
+      def company
+        access_token.get("/api2.php/#{ access_token.params['org'] }/company_profile/1")
       end
     end
   end
